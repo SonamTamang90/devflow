5536648fbfc71b19791ba1831451b7f40adda61b
stamang <stamang.mrbhutan90@gmail.com>
2025-04-05 01:39:35 +0800

    Implement local searchbar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,24 +1,89 @@
-import { auth, signOut } from "@/auth";
+import { signOut } from "@/auth";
+import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
+import { SearchParams } from "next/dist/server/request/search-params";
+import Link from "next/link";
 
-const Home = async () => {
-  const session = await auth();
-  console.log(session);
+const questions = [
+  {
+    _id: "1",
+    title: "How to learn React?",
+    description: "I want to learn React, can anyone help me?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "JavaScript" },
+    ],
+    author: { _id: "1", name: "John Doe" },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+  {
+    _id: "2",
+    title: "How to learn JavaScript?",
+    description: "I want to learn JavaScript, can anyone help me?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "JavaScript" },
+    ],
+    author: { _id: "1", name: "John Doe" },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+];
+
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string | string[] }>;
+}
+
+const Home = async ({ searchParams }: SearchParams) => {
+  const { query = "" } = await searchParams;
+  const filteredQuestions = questions.filter((question) =>
+    question.title.toLowerCase().includes(query?.toLowerCase())
+  );
   return (
     <>
-      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Log out</Button>
-      </form>
+      <section className="w-full flex flex-col-reverse sm:flex-row justify-between sm:items-center gap-4">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button
+          className="primary-gradient min-h-[45px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Quesiton</Link>
+        </Button>
+      </section>
+      <section className="mt-11">
+        <LocalSearch
+          route="/"
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+        />
+      </section>
+      {/* HomeFilter */}
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {filteredQuestions.map((question) => (
+          <h1 key={question._id} className="">
+            {question.title}
+          </h1>
+        ))}
+      </div>
     </>
   );
 };
 
 export default Home;
+
+// <form
+//         className="px-10 pt-[100px]"
+//         action={async () => {
+//           "use server";
+//           await signOut({ redirectTo: ROUTES.SIGN_IN });
+//         }}
+//       >
+//         <Button type="submit">Log out</Button>
+//       </form>
