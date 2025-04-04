dbc46e0a712b583d5b80894fa165a43f8306f4bb
stamang <stamang.mrbhutan90@gmail.com>
2025-04-04 23:54:25 +0800

    Implement right sidebar

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
+import { techMap } from "@/constants/techMap";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const getDeviconsClassName = (techName: string) => {
+  const normalizedName = techName.replace(/[.]/g, "").toLowerCase();
+
+  return techMap[normalizedName]
+    ? `${techMap[normalizedName]} colored`
+    : "devicon-devicon-plain";
+};
