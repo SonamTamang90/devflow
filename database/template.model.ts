c56f02d88f01bc4460ecf77a047c8026ae14fd16
stamang <stamang.mrbhutan90@gmail.com>
2025-04-17 18:40:41 +0800

    Implement interaction model

--- database/template.model.ts
+++
@@ -1,9 +0,0 @@
-import { model, models, Schema } from "mongoose";
-
-export interface IModel {}
-
-const ModelSchema = new Schema<IModel>({}, { timestamps: true });
-
-const Model = models?.account || model<IModel>("Account", ModelSchema);
-
-export default Model;
